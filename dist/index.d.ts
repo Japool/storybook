aeccd3f8582862496ae542f601d1806495b7f49f
Jardiel Enrique Pool Hu (Programador Analista) <poolhj@autocar.com>
2022-12-23 15:11:50 -0500

    fix dropdown-agregados

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,7 @@
 export { Button } from "./stories/Button";
 export { Header } from "./stories/Header";
-export { MyLabel } from "./components/MyLabel";
 export { Page } from "./stories/Page";
+export { MyLabel } from "./components/MyLabel";
+export { Dropdown } from "./components/Dropdown";
+export { InputField } from "./components/InputField";
+export { MostrarErrorCampo } from "./components/MostrarErrorCampo";
